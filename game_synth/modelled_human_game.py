0e18d97a628498052a091a0e7f4f1a8bbbdaed7c
Georg Schuppe <georg.schuppe@gmail.com>
2022-03-30 11:51:20 +0200

    feat: synthesis game with human model

--- game_synth/modelled_human_game.py
+++ game_synth/modelled_human_game.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import queue
 
+from game_synth.helpers import sog_fits_to_guard
 
-def create_game(mdp_r, dfa, mdp_h):
+
+def create_game(mdp_r, mdp_h, dfa):
     synth = nx.DiGraph()
     synth.graph['acc'] = []  # list of accepting states
 
@@ -36,5 +38,84 @@ def create_game(mdp_r, dfa, mdp_h):
     # work the queue
     while not que.empty():
         synth_from = que.get()
+        mdp_r_from = synth_from[0]
+        mdp_h_from = synth_from[1]
+        dfa_from = synth_from[2]
+
+        assert synth.nodes[synth_from]['player'] in [1, 2, 0], "Each state need to belong to player 1,2 or 0!"
+
+        # player 1 states, robot moves
+        if synth.nodes[synth_from]['player'] == 1:
+            # for all possible robot moves
+            for robot_succ in mdp_r.successors(mdp_r_from):
+                # this should be a probabilistic state in the robot mdp
+                assert mdp_r.nodes[robot_succ]['player'] == 0
+                # add the new state to the synthesis product and connect it
+                synth_succ = (robot_succ, mdp_h_from, dfa_from)
+                if not synth.has_node(synth_succ):
+                    synth.add_node(synth_succ, player=0)
+                    que.put(synth_succ)  # put new states in queue
+                action_lbl = mdp_r.edges[mdp_r_from, robot_succ]['act']
+                synth.add_edge(synth_from, synth_succ, act=action_lbl)
+
+        # player 2 states, human moves
+        if synth.nodes[synth_from]['player'] == 2:
+            # for all possible human moves
+            for human_succ in mdp_h.successors(mdp_h_from):
+                # this should be a probabilistic state in the human mdp
+                assert mdp_h.nodes[human_succ]['player'] == 0
+                # add the new state to the synthesis product and connect it
+                synth_succ = (mdp_r_from, human_succ, dfa_from)
+                if not synth.has_node(synth_succ):
+                    synth.add_node(synth_succ, player=0)
+                    que.put(synth_succ)  # put new states in queue
+                action_lbl = mdp_h.edges[mdp_h_from, human_succ]['act']
+                synth.add_edge(synth_from, synth_succ, act=action_lbl)
+
+        # probabilistic states
+        if synth.nodes[synth_from]['player'] == 0:
+            assert sum(1 for _ in synth.predecessors(synth_from)) == 1
+            pred = next(synth.predecessors(synth_from))
+
+            # case 1: after p1 state, just resolve probabilistic event for robot
+            if synth.nodes[pred]['player'] == 1:
+                for robot_succ in mdp_r.successors(mdp_r_from):
+                    # this should be a player1 state in the robot mdp
+                    assert mdp_r.nodes[robot_succ]['player'] == 1
+                    # add the new state to the synthesis product and connect it
+                    synth_succ = (robot_succ, mdp_h_from, dfa_from)
+                    if not synth.has_node(synth_succ):
+                        synth.add_node(synth_succ, player=2)
+                        que.put(synth_succ)  # put new states in queue
+                    prob = mdp_r.edges[mdp_r_from, robot_succ]['prob']
+                    synth.add_edge(synth_from, synth_succ, prob=prob)
+
+            # case 2: after p2 state, resolve probabilistic event for human, also update DFA
+            elif synth.nodes[pred]['player'] == 2:
+                for human_succ in mdp_h.successors(mdp_h_from):
+                    # this should be a player1 state in the human mdp
+                    assert mdp_h.nodes[human_succ]['player'] == 1
+                    # get the resulting joined ap
+                    cur_ap = mdp_r.nodes[mdp_r_from]['ap'] + mdp_h.nodes[human_succ]['ap']
+
+                    # based on new observation (cur_ap), update the dfa state
+                    for dfa_succ in dfa.successors(dfa_from):
+                        # check if config matches at least one dfa guard, skip non-matching
+                        dfa_guard = dfa.edges[dfa_from, dfa_succ]['guard']
+                        matched_guards = sog_fits_to_guard(cur_ap, dfa_guard, joined_ap, spec_ap)
+                        if len(matched_guards) == 0:
+                            continue
+
+                        # add the new state to the synthesis product and connect it
+                        synth_succ = (mdp_r_from, human_succ, dfa_succ)
+                        if not synth.has_node(synth_succ):
+                            synth.add_node(synth_succ, player=1, ap=cur_ap)
+                            que.put(synth_succ)  # put new states in queue
+                            if dfa_succ in dfa.graph['acc']:
+                                synth.graph['acc'].append(synth_succ)
+                        prob = mdp_h.edges[mdp_h_from, human_succ]['prob']
+                        synth.add_edge(synth_from, synth_succ, prob=prob)
+            else:
+                assert False, "probabilistic state with invalid predecessor"
 
     return synth
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from py4j.protocol import Py4JNetworkError
 from models.corridor import corridor_mdp
 from ltlf2dfa_nx.mona2nx import to_nxgraph
 from ltlf2dfa_nx.parse_ltlf import to_mona
-from game_synth.modelless_human_game import create_game
+# from game_synth.modelless_human_game import create_game
+from game_synth.modelled_human_game import create_game
 from game_synth.helpers import remove_edges
 from advisers.safety import minimal_safety_edges
 from advisers.fairness import minimal_fairness_edges
@@ -13,11 +14,13 @@ if __name__ == "__main__":
         prism_handler = PrismBridge()
         print("Successfully connected to PRISM java gateway!")
 
-        mdp = corridor_mdp("_r", "end_top")
+        robot_model = corridor_mdp("_r", "end_top")
         human_model = corridor_mdp("_h", "end_bot")
         spec = "F(end_top_r) && G(!(crit_r && crit_h))"
         dfa = to_nxgraph(to_mona(spec))
-        synth = create_game(mdp, dfa)
+        synth = create_game(robot_model, human_model, dfa)
+
+        # print(*synth.nodes(data=True), sep='\n')
         safety_edges = minimal_safety_edges(synth, prism_handler)
         remove_edges(synth, safety_edges)
         fairness_edges = minimal_fairness_edges(synth, prism_handler)
